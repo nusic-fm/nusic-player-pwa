26625c869838eee3e2980b84da90040b022f64e6
logesh2496 <logesh.r24@gmail.com>
2023-01-05 13:07:01 +0530

    fix: add pwa manifest ref

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,6 +11,7 @@ export default class MyDocument extends Document {
         <Head>
           {/* PWA primary color */}
           <meta name="theme-color" content={theme.palette.primary.main} />
+          <link rel="manifest" href="/manifest.json"></link>
           {/* <link
             rel="stylesheet"
             href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
